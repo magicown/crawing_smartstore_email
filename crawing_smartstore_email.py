21ab4980e481bc22cc4f1688535d63aef020fa04
james kim <phpprogram47@gmail.com>
2020-12-22 18:59:53 +0900

    목록만들기와 url 만들기

--- crawing_smartstore_email.py
+++ crawing_smartstore_email.py
@@ -35,11 +35,16 @@ def get_html_source(URL) :
     html_doc = driver.page_source
     soup = BeautifulSoup(html_doc, 'html.parser')
     list_div = soup.select('.basicList_item__2XT81')
-    print(list_div)
+    print(len(list_div))
     for list_d in list_div :
-        print(list_d)
+        # print(list_d)
+        try :
+            list_url.append(list_d.get_text())
+            # print('try 실행됨')
+        except :
+            break
     # //*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/ul/div/div[1]/li/div/div[2]/div[1]/a
-
+    # print(list_url)
 
 
 options = chrome_option()
@@ -62,11 +67,27 @@ while start < 2:
     if new_height == last_height:
         try:
             start = start + 1
-            get_html_source("https://search.shopping.naver.com/search/all?frm=NVSHOVS&origQuery=%EC%9D%B8%ED%85%8C%EB%A6%AC%EC%96%B4&pagingIndex=" + str(start) + "&pagingSize=1000&productSet=overseas&query=%EC%9D%B8%ED%85%8C%EB%A6%AC%EC%96%B4&sort=rel&timestamp=&viewType=list")
+            
         except:
             break
-    # last_height = new_height
+    last_height = new_height
 
+
+list_d = ""
+list_e = []
+list_url = []
+html_doc = driver.page_source
+soup = BeautifulSoup(html_doc, 'html.parser')
+list_div = soup.select('.basicList_item__2XT81')
+for i in list_div :
+    
+    list_d = i.select('.basicList_title__3P9Q7')
+    for j in list_d :
+        list_e.append(j.get_text())
+        list_url.append(j.select('a'))
+
+
+print(list_url)
 
 # res = True
 # URL = "https://smartstore.naver.com/blossom82/products/2643752178?NaPm=ct%3Dkizkrtyw%7Cci%3Df8e9652b315e7323334bce1297266b74551404cd%7Ctr%3Dslsl%7Csn%3D669077%7Chk%3D912eb9754eebee0f42aa2ccb46647a25a0db1d43"
